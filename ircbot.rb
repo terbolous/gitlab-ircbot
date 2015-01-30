fe9c689f5bea61d336fa7670c322c92d0a847596
Erik Weber <erik@huug.no>
2015-01-30 12:59:14 +0100

    rewrite commit vs tag check

--- ircbot.rb
+++ ircbot.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Application
     if $config['gitlab'].include? project_name
       ch = $config['gitlab'][project_name]['channel']
       send = nil
-      if data.include? commits
+      unless data['ref'].include? 'ref/tags'
         # FIXME: chec if this is commit or tag
         commit = data['commits'][0]
         commit_message = commit['message'].gsub(/\n/," ")
